3c5a75268c58cf28daa5de7ae2ad4906102d976a
ID-TALEB <abdessamad.id-taleb@cgi.com>
2024-11-01 14:36:11 +0100

    31/10/20241416

--- src/app/services/coins-service.service.ts
+++ src/app/services/coins-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
@@ -7,36 +7,26 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class CoinsService {
-  private apiUrl = environment.apiUrl + 'coins'; // Replace with your API URL
+  private apiUrl = environment.apiUrl+'coins'; // Remplacez par l'URL de votre API
 
   constructor(private http: HttpClient) {}
 
-  // Common headers for all requests
-  private getHeaders() {
-    return new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*', // This is typically handled on the server-side
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS' // Also typically handled on the server-side
-    });
-  }
-
-  // Get all coins
+  // Obtenir tous les coins
   getCoins(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}`, { headers: this.getHeaders() });
+    return this.http.get<any>(`${this.apiUrl}`);
   }
 
-  // Update a coin
+  // Mettre à jour le prix d'un coin
   updateCoin(coin: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${coin.id}`, coin, { headers: this.getHeaders() });
+    const headers = { 'Content-Type': 'application/json' };
+    return this.http.put(`${this.apiUrl}/${coin.id}`, coin, { headers });
   }
   
-  // Add a coin
   addCoin(coinRequest: { name: string; litnom: string; price: number }): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}`, coinRequest, { headers: this.getHeaders() });
+    return this.http.post<any>(`${this.apiUrl}`, coinRequest);
   }
 
-  // Delete a coin
   deleteCoin(coinId: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${coinId}`, { headers: this.getHeaders() });
+    return this.http.delete(`${this.apiUrl}/delete/${coinId}`);
   }
 }
